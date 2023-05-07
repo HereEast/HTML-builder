f574e38123bf9bd1c8368d8f7ae611af431212a0
Here East <nastiapiven@gmail.com>
2023-05-07 15:25:04 +0200

    refactor: update 01-read-file console log result

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,12 +1,15 @@
 const fs = require("fs");
 const path = require("path");
 
+const { stdout } = process;
+
 const readableStream = fs.createReadStream(
     path.join(__dirname, "text.txt"), 
     "utf-8"
 );
 
-const { stdout } = process;
-
-readableStream.on("data", data => stdout.write(data));
+readableStream.on("data", data => {
+    stdout.write("⬇️ Result:\n");
+    stdout.write(data);
+});
 readableStream.on("error", error => console.error(error.message));
